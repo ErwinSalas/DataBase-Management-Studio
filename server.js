ac5245fdd3b43d9b8d1b3b9a01abbcb6eec705d1
root <root@localhost.localdomain>
2018-03-25 12:38:59 -0500

    Deploy master 3

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
 
+var server_port = process.env.YOUR_PORT || process.env.PORT || 5000;
 
-connect().use(serveStatic(__dirname)).listen(5080, function(){
-    console.log('Server running on 8080...');
+connect().use(serveStatic(__dirname)).listen(server_port, function(){
+    console.log('Server running on ...'+ server_port);
 });
